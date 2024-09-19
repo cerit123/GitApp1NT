e6b6ee8b700cc8aa29eed4aa4b9a658acac96b1b
cerit123 <hafizeozcerit@gmail.com>
2024-09-19 20:07:05 +0100

    v4

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,6 +4,8 @@ public class Runner {
         System.out.println("version 1");
         System.out.println("version 2");
         System.out.println("version 2 nin devami");
+        System.out.println("versiyon 3");
+        System.out.println("versiyon 4");
 
 
     }
